844e3d45848db8c3d96320f5cea7200932a7e53d
shyricecake <leelucky1@naver.com>
2025-02-06 02:07:24 +0900

    모험가 길드 그리디 알고리즘

--- 이코테/greedy.py
+++ 이코테/greedy.py
@@ -71,3 +71,67 @@ for i in range(1, len(data)):
 
 print(result)
 
+
+# ---------
+# 4. 모험가 길드 왜 그리디 알고리즘인가?
+
+n = int(input())
+data = list(map(int,input.split()))
+data.sort()
+
+result = 0
+count = 0
+
+for i in data :
+    count += 1
+    if count >= i:
+        result += 1
+        count =0
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
